1fcfcc5beb15e18691f93b5b15c301ca78d18aab
Antonio Gabriel <prof.antoniogabriel@alixar.org>
2023-02-24 11:12:19 +0100

    Fix import core

--- src/app/core/material/material.module.ts
+++ src/app/core/material/material.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { MatCardModule } from '@angular/material/card';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    MatCardModule
   ],
   exports: [
-    
+    MatCardModule
   ]
 })
 export class MaterialModule { }
--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { FormsModule } from '@angular/forms';
+import { CoreModule } from '../core/core.module';
 
 
 
@@ -14,7 +15,8 @@ import { FormsModule } from '@angular/forms';
   ],
   imports: [
     CommonModule,
-    FormsModule
+    FormsModule,
+    CoreModule
   ],
   exports: [
     
